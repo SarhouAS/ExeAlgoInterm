41a5f5ecae53588ea28c82afac2fd1bc6bcd496a
Aymane Sarhou <sarhouaymane2004@gmail.com>
2026-02-12 17:07:17 +0100

    Exercice9

---
+++ Exercice9.py
@@ -0,0 +1,19 @@
+def remplacer_cellule(tableau, ancienne_valeur, nouvelle_valeur):
+    for i in range(len(tableau)):
+        for j in range(2):  # exactement 2 colonnes
+            if tableau[i][j] == ancienne_valeur:
+                tableau[i][j] = nouvelle_valeur
+
+    return tableau
+
+tableau = [
+    ["Alice", 15],
+    ["Bob", 12],
+    ["Clara", 15]
+]
+
+print("Avant :", tableau)
+
+resultat = remplacer_cellule(tableau, 15, 20)
+
+print("Après :", resultat)
