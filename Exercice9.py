557475831273f029aae3db14d50a3b66774f79de
Aymane Sarhou <sarhouaymane2004@gmail.com>
2026-02-12 17:07:50 +0100

    Exercice9

--- Exercice9.py
+++ Exercice9.py
@@ -1,6 +1,6 @@
 def remplacer_cellule(tableau, ancienne_valeur, nouvelle_valeur):
     for i in range(len(tableau)):
-        for j in range(2):  # exactement 2 colonnes
+        for j in range(2):
             if tableau[i][j] == ancienne_valeur:
                 tableau[i][j] = nouvelle_valeur
 
