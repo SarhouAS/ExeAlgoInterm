fbcad3d552161e2c6b6c2bca8d986ee8bf4f0d4f
Aymane Sarhou <sarhouaymane2004@gmail.com>
2026-02-12 16:48:58 +0100

    Exercice7

---
+++ Exerciceè.py
@@ -0,0 +1,18 @@
+def remplacer_par_zero(chaine, n):
+    nouvelle_chaine = ""
+
+    for i in range(len(chaine)):
+        if (i + 1) % n == 0:  # chaque nème position
+            nouvelle_chaine += "0"
+        else:
+            nouvelle_chaine += chaine[i]
+
+    return nouvelle_chaine
+
+
+# Test
+chaine = input("Entrez une chaîne : ")
+n = int(input("Entrez n : "))
+
+resultat = remplacer_par_zero(chaine, n)
+print(f"Résultat : {resultat}")
